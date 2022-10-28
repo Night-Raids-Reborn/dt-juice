f9f3bf0d2c619d7907e0392606dabdd7ff11c776
darknius2 <darknius2@gmail.com>
2022-10-28 00:48:44 +0000

    awaken bringup

--- awaken_juice.mk
+++ awaken_juice.mk
@@ -7,21 +7,15 @@
 # Inherit from juice device
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common Fluid X stuff.
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+# Inherit some common awaken X stuff.
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := fluid_juice
+PRODUCT_NAME := awaken_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Juice
 PRODUCT_MANUFACTURER := Xiaomi
-
-# Fluid flags
-FLUID_BUILD_TYPE := UNOFFICIAL
-PRODUCT_PRODUCT_PROPERTIES += \
-	ro.fluid.maintainer=Darknius \
-	ro.fluid.cpu=BENGAL
 
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
