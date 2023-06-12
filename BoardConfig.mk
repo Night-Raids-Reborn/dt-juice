a0d3393fa1b31e23b045cee28d2cc1aa6f2d04d5
darknius09 <darkniuslightness@gmail.com>
2023-06-12 02:54:36 +0000

    juice: use qti power hal

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,6 +150,7 @@ BUILD_WITHOUT_VENDOR := true
 # Power
 TARGET_USES_INTERACTION_BOOST := true
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Platform
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno610
--- device.mk
+++ device.mk
@@ -142,7 +142,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.juice
+    android.hardware.power-service.qti \
+    vendor.qti.hardware.perf@2.0 \
+    vendor.qti.hardware.perf@2.1 \
+    vendor.qti.hardware.perf@2.2
 
 # Properties
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
